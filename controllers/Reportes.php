9ccf3ea6702a242af121a826c5017a6d769723ae
Mauricio Abisay <mauricioabisay.lopez@gmail.com>
2018-11-10 01:46:55 -0600

    Reportes skeleton

--- controllers/Reportes.php
+++ controllers/Reportes.php
@@ -3,11 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Reportes extends CI_Controller {
     public function index() {
-        
+        $this->load->model('campus/campus_model');
     }
 
     public function alumnos_reinscritos() {
+        $planteles = $this->campus_model->planteles();
+        $years = $this->campus_model->ciclos_academicos();
+        $periodos = $this->campus_model->periodos_academicos();
 
+        $modalidades = $this->campus_model->modalidades();
+        $niveles = $this->campus_model->niveles_estudios();
+        $carreras = $this->campus_model->programas_educativos();
+
+        if ( $this->input->post() ) {
+            $this->form_validation->set_rules('plantel', 'Plantel', 'required');
+            $this->form_validation->set_rules('modalidad', 'Modalidad', 'required');
+            $this->form_validation->set_rules('nivel', 'Nivel', 'required');
+            $this->form_validation->set_rules('years', 'Año', 'required');
+            $this->form_validation->set_rules('periodo', 'Periodo', 'required');
+            if ( this->form_validation->run() ) {
+                //Here should implement Reportes/alumnos_reinscritos_detalle.php
+                return $this->load->view(
+                    'view', 
+                    compact()
+                );
+            }
+        }
+        return $this->load->view(
+            '',
+            compact(
+                'planteles',
+                'years',
+                'periodos',
+                'modalidades',
+                'niveles',
+                'carreras'
+            )
+        );    
     }
 
     public function alumnos_nuevos() {
